3e1c79442d292c9f25e16742d0ccd9dce0a88e9d
benrnz <ben@rees.biz>
2016-05-08 15:47:26 +1200

    Add unit test project with some experimental tests.  NUnit added @ 2.6.4

---
+++ CorporateBsGenerator.UnitTest/ExperimentalTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CorporateBsGenerator.UnitTest
+{
+    [TestFixture]
+    public class ExperimentalTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(5, 2 + 2);
+        }
+    }
+}
