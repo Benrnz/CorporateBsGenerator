d9d6a13c06d8d6d7c775063550b4755081183926
benrnz <ben@rees.biz>
2016-05-08 14:20:23 +1200

    New icon, adjusted some droid project settings, and testing build hook

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace CorporateBsGenerator.Droid
 {
-	[Activity (Label = "Corporate BS Generator", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "Corporate BS Generator", Icon = "@drawable/ic_launcher", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
