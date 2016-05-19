5c030b0622ba0495bef2a20e35bf84e4a3b7e260
benrnz <ben@rees.biz>
2016-05-20 10:57:41 +1200

    Add Unit Test Project from Xamarin Studio (previously done with Vs2015)

---
+++ UnitTest/BuildTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UnitTest
+{
+	[TestFixture ()]
+	public class BuildTest
+	{
+		[Test()]
+		public void TestCase1()
+		{
+			Assert.True (true);
+		}
+	}
+}
+
