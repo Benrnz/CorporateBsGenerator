e8b40704f42fe04a17bc5ab46db762f88a66731b
benrnz <ben@rees.biz>
2016-05-23 22:08:25 +1200

    Reference PCL and use a class in a test

--- UnitTest/BuildTest.cs
+++ UnitTest/BuildTest.cs
@@ -1,16 +1,22 @@
-﻿using NUnit.Framework;
-using System;
+﻿using CorporateBsGenerator.Services;
+using NUnit.Framework;
 
 namespace UnitTest
 {
-	[TestFixture ()]
-	public class BuildTest
-	{
-		[Test()]
-		public void TestCase1()
-		{
-			Assert.True (true);
-		}
-	}
-}
+    [TestFixture]
+    public class BuildTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            Assert.True(true);
+        }
 
+        [Test]
+        public void TestCase2()
+        {
+            var subject = new GeneratorService();
+            Assert.IsFalse(string.IsNullOrWhiteSpace(subject.Generate()));
+        }
+    }
+}
