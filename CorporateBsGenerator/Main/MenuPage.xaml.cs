f0d20f53f8e6b8824e614abafde64339ec4904cb
benrnz <ben@rees.biz>
2016-06-05 14:29:04 +1200

    Wire up new view model

--- CorporateBsGenerator/Main/MenuPage.xaml.cs
+++ CorporateBsGenerator/Main/MenuPage.xaml.cs
@@ -10,16 +10,7 @@ namespace CorporateBsGenerator.Main
     {
         public MenuPage()
         {
-            List<HomeMenuItem> menuItems;
             InitializeComponent();
-
-            this.ListViewMenu.ItemsSource = menuItems = new List<HomeMenuItem>
-            {
-                new HomeMenuItem { Title = "About", MenuType = MenuType.About, Icon = "about.png" },
-                new HomeMenuItem { Title = "Generator", MenuType = MenuType.Generator, Icon = "tdl.png" } // TODO Need an icon here
-            };
-
-            this.ListViewMenu.SelectedItem = menuItems[1];
         }
 
         private async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
